02a4c557682084351d1c078534013a41aac37153
QwertyKeyb0b <a.ferrulli27@studenti.uniba.it>
2024-10-29 18:17:24 +0100

    added class to read datasets

--- S_p_o_t_i_AI/main.py
+++ S_p_o_t_i_AI/main.py
@@ -2,7 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
-import requests
+import dataset
 
 # Configura le tue credenziali qui
 
@@ -25,29 +25,16 @@ def get_saved_tracks():
         print(f"{idx + 1}: {track['name']} di {track['artists'][0]['name']}")
 
 
-def get_mbrz_data():
-    track_name = "Time"
-    artist_name = "Pink Floyd"
-
-    # Costruisci l'URL della richiesta
-    url = f"https://musicbrainz.org/ws/2/recording/?query=track:{track_name}+artist:{artist_name}&fmt=json"
-    response = requests.get(url)
-    data = response.json()
-
-    # Estrai l'MBID del primo risultato
-    if data['recordings']:
-        mbid = data['recordings'][0]['id']
-        print("MBID:", mbid)
-    else:
-        print("Nessun risultato trovato.")
-
-    low_level_data = requests.get(f"https://acousticbrainz.org/{mbid}/low-level").json()
-    print(low_level_data)
-    print("\n")
-    high_level_data = requests.get(f"https://acousticbrainz.org/{mbid}/high-level").json()
-    print(high_level_data)
-
-
 if __name__ == "__main__":
     get_saved_tracks()
-    get_mbrz_data()
+    print("\n\n\n")
+    dataByArtist = dataset.Dataset("data_by_artist.csv")
+    print(dataByArtist.getDataset())
+    print("\n\n\n")
+    print(dataByArtist.getDataFrame(["artists", "popularity"]))
+    dataByArtist.normalizeColumn("popularity")
+    print(dataByArtist.getDataset())
+    print("\n\n\n")
+    dataByArtist.EDA()
+
+
