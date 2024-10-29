85de6141a424af93290480e9f36e1780bf5641d6
QwertyKeyb0b <a.ferrulli27@studenti.uniba.it>
2024-10-29 12:56:32 +0100

    code fixed not to show credentials

--- S_p_o_t_i_AI/main.py
+++ S_p_o_t_i_AI/main.py
@@ -1,15 +1,17 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+import requests
 
 # Configura le tue credenziali qui
-CLIENT_ID = 'cc6fead3f5504f4d8e88802f8f1184be'
-CLIENT_SECRET = 'd2ffd3f1fdc1499dbbd4f04667ffd7c7'
+load_dotenv()
 REDIRECT_URI = 'http://localhost:8888/callback'
 SCOPE = 'user-library-read'  # Cambia il scope in base alle tue esigenze
 
 # Autenticazione
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                               client_secret=CLIENT_SECRET,
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv("CLIENT_ID"),
+                                               client_secret=os.getenv("CLIENT_SECRET"),
                                                redirect_uri=REDIRECT_URI,
                                                scope=SCOPE))
 
@@ -22,5 +24,30 @@ def get_saved_tracks():
         print(f"{idx + 1}: {track['name']} di {track['artists'][0]['name']}")
 
 
+def get_mbrz_data():
+    track_name = "Time"
+    artist_name = "Pink Floyd"
+
+    # Costruisci l'URL della richiesta
+    url = f"https://musicbrainz.org/ws/2/recording/?query=track:{track_name}+artist:{artist_name}&fmt=json"
+    response = requests.get(url)
+    data = response.json()
+
+    # Estrai l'MBID del primo risultato
+    if data['recordings']:
+        mbid = data['recordings'][0]['id']
+        print("MBID:", mbid)
+    else:
+        print("Nessun risultato trovato.")
+
+    low_level_data = requests.get(f"https://acousticbrainz.org/{mbid}/low-level").json()
+    print(low_level_data)
+    print("\n")
+    high_level_data = requests.get(f"https://acousticbrainz.org/{mbid}/high-level").json()
+    print(high_level_data)
+
+
+
 if __name__ == "__main__":
     get_saved_tracks()
+    get_mbrz_data()
